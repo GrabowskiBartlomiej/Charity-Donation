0ae4afb1fe316520a7c06e335dabbd205bc486b2
GrabowskiBartlomiej <bartlomiejgrabowski@virgilio.it>
2021-05-21 11:32:40 +0200

    #1 Basic project configuration + Entities creation

---
+++ src/main/java/pl/coderslab/charity/Repository/InstitutionRepo.java
@@ -0,0 +1,10 @@
+package pl.coderslab.charity.Repository;
+
+import org.springframework.stereotype.Repository;
+import pl.coderslab.charity.Entity.Institution;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+@Repository
+public interface InstitutionRepo extends JpaRepository<Institution, Long> {
+
+}
