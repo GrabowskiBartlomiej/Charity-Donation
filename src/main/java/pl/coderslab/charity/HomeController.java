4ac9959bc99be264ec6bbbef1fb4199e94a0c78b
GrabowskiBartlomiej <bartlomiejgrabowski@virgilio.it>
2021-05-21 12:16:25 +0200

    #2 Institution list with foreach on the landing page.

--- src/main/java/pl/coderslab/charity/HomeController.java
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -1,15 +1,29 @@
 package pl.coderslab.charity;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.coderslab.charity.Entity.Institution;
+import pl.coderslab.charity.Repository.InstitutionRepo;
 
 
 @Controller
 public class HomeController {
 
+    private InstitutionRepo institutionRepo;
+
+    @Autowired
+    public HomeController(InstitutionRepo institutionRepo) {
+        this.institutionRepo = institutionRepo;
+    }
+
     @RequestMapping("/")
     public String homeAction(Model model) {
+        model.addAttribute("institutions", institutionRepo.findAll());
+        for(Institution institution : institutionRepo.findAll()){
+            System.out.println(institution.getName());
+        }
         return "index";
     }
 }
