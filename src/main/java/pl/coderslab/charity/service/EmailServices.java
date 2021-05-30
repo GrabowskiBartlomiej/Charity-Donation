f148861ab13e5dd6fe5a5f92d66aa496c153841f
GrabowskiBartlomiej <bartlomiejgrabowski@virgilio.it>
2021-05-30 18:12:40 +0200

    #13 Email sender implemented.

---
+++ src/main/java/pl/coderslab/charity/service/EmailServices.java
@@ -0,0 +1,25 @@
+package pl.coderslab.charity.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailServices{
+
+    private JavaMailSender mailSender;
+
+    @Autowired
+    public EmailServices(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendNormalMessage(String to, String subject, String text){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        mailSender.send(message);
+    }
+}
